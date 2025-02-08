35747acbda4ca37298e908d67cc4adc3beffc888
peterdinis <petter1@pobox.sk>
2025-02-08 18:43:18 +0100

    infra: Update auth.ts action

--- app/_actions/auth.ts
+++ app/_actions/auth.ts
@@ -1,12 +1,12 @@
-"use server"
-
+"use server";
 import { encodeBase32LowerCaseNoPadding, encodeHexLowerCase } from "@oslojs/encoding";
 import { sha256 } from "@oslojs/crypto/sha2";
-import type { User, Session } from "@prisma/client";
-import { db } from "@/lib/db";
 import { cookies } from "next/headers";
+import { cache } from "react";
+import { db } from "@/lib/db";
+import { Session, User } from "@prisma/client";
 
-export function generateSessionToken(): string {
+export async function generateSessionToken(): Promise<string> {
 	const bytes = new Uint8Array(20);
 	crypto.getRandomValues(bytes);
 	const token = encodeBase32LowerCaseNoPadding(bytes);
@@ -20,10 +20,9 @@ export async function createSession(token: string, userId: number): Promise<Sess
 		userId,
 		expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)
 	};
-	await db
-		.session.create({
-			data: session
-		});
+	await db.session.create({
+		data: session
+	});
 	return session;
 }
 
@@ -56,7 +55,13 @@ export async function validateSessionToken(token: string): Promise<SessionValida
 			}
 		});
 	}
-	return { session, user };
+
+    const safeUser = {
+        ...user,
+        passwordHash: undefined,
+    }
+
+	return { session, user: safeUser };
 }
 
 export async function invalidateSession(sessionId: string): Promise<void> {
@@ -64,28 +69,125 @@ export async function invalidateSession(sessionId: string): Promise<void> {
 }
 
 export type SessionValidationResult =
-	| { session: Session; user: User }
+	| { session: Session; user: Omit<User, "passwordHash"> }
 	| { session: null; user: null };
 
 
-	export async function setSessionTokenCookie(token: string, expiresAt: Date): Promise<void> {
-		const cookieStore = await cookies();
-		cookieStore.set("session", token, {
-			httpOnly: true,
-			sameSite: "lax",
-			secure: process.env.NODE_ENV === "production",
-			expires: expiresAt,
-			path: "/"
-		});
+
+
+
+/* Cookies */
+export async function setSessionTokenCookie(token: string, expiresAt: Date): Promise<void> {
+	const cookieStore = await cookies();
+	cookieStore.set("session", token, {
+		httpOnly: true,
+		sameSite: "lax",
+		secure: process.env.NODE_ENV === "production",
+		expires: expiresAt,
+		path: "/"
+	});
+}
+
+export async function deleteSessionTokenCookie(): Promise<void> {
+	const cookieStore = await cookies();
+	cookieStore.set("session", "", {
+		httpOnly: true,
+		sameSite: "lax",
+		secure: process.env.NODE_ENV === "production",
+		maxAge: 0,
+		path: "/"
+	});
+}
+
+export const getCurrentSession = cache(async (): Promise<SessionValidationResult> => {
+	const cookieStore = await cookies();
+	const token = cookieStore.get("session")?.value ?? null;
+	if (token === null) {
+		return { session: null, user: null };
 	}
-	
-	export async function deleteSessionTokenCookie(): Promise<void> {
-		const cookieStore = await cookies();
-		cookieStore.set("session", "", {
-			httpOnly: true,
-			sameSite: "lax",
-			secure: process.env.NODE_ENV === "production",
-			maxAge: 0,
-			path: "/"
-		});
-	}
+	const result = await validateSessionToken(token);
+	return result;
+});
+
+
+/* User register, login, logout */
+export const hashPassword = async (password: string) => {
+    return encodeHexLowerCase(sha256(new TextEncoder().encode(password)));
+}
+
+export const verifyPassword = async (password: string, hash: string) => {
+    const passwordHash = await hashPassword(password);
+    return passwordHash === hash;
+}
+
+export const registerUser = async (email: string, password: string) => {
+    const passwordHash = await hashPassword(password);
+    try {
+        const user = await db.user.create({
+            data: {
+                email,
+                passwordHash,
+            }
+        });
+
+        const safeUser = {
+            ...user,
+            passwordHash: undefined,
+        };
+
+        return {
+            user: safeUser,
+            error: null,
+        }
+    } catch(e) {
+        return {
+            user: null,
+            error: "Failed to register user"
+        }
+    }
+}
+
+export const loginUser = async (email: string, password: string) => {
+    const user = await db.user.findUnique({
+        where: {
+            email: email,
+        }
+    });
+
+    if(!user) {
+        return {
+            user: null,
+            error: "User not found",
+        }
+    }
+
+    const passwordValid = await verifyPassword(password, user.passwordHash);
+    if(!passwordValid) {
+        return {
+            user: null,
+            error: "Invalid password",
+        }
+    }
+
+    const token = await generateSessionToken();
+    const session = await createSession(token, user.id);
+    await setSessionTokenCookie(token, session.expiresAt);
+
+    const safeUser = {
+        ...user,
+        passwordHash: undefined,
+    };
+
+    return {
+        user: safeUser,
+        error: null,
+    }
+}
+
+export const logoutUser = async () => {
+    const session = await getCurrentSession();
+    if(session.session?.id) {
+        await invalidateSession(session.session.id);
+    }
+    await deleteSessionTokenCookie();
+}
