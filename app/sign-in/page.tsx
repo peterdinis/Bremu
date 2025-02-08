60955293339af5a1ba84adfeada78696813bdb0f
peterdinis <petter1@pobox.sk>
2025-02-08 18:57:47 +0100

    feat: Create new auth forms

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,39 @@
+import { redirect } from 'next/navigation';
+import zod from 'zod';
+import SigninForm from '../_components/auth/SigninForm';
+import { getCurrentSession, loginUser } from '../_actions/auth';
+
+const SignInSchema = zod.object({
+    email: zod.string().email(),
+    password: zod.string().min(5),
+})
+
+const SignInPage = async () => {
+    const { user } = await getCurrentSession();
+
+    if (user) {
+        return redirect('/');
+    }
+
+    const action = async (prevState: any, formData: FormData) => {
+        "use server";
+        const parsed = SignInSchema.safeParse(Object.fromEntries(formData));
+        if(!parsed.success) {
+            return {
+                message: "Invalid form data",
+            };
+        }
+
+        const { email, password } = parsed.data;
+        const { user, error } = await loginUser(email, password);
+        if(error) {
+            return { message: error };
+        } else if(user) {
+            return redirect("/");
+        }
+    }
+
+    return <SigninForm action={action} />;
+};
+
+export default SignInPage;
