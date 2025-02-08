91c300b02a17266d9190883d2bcae1141c0764be
peterdinis <petter1@pobox.sk>
2025-02-08 18:47:37 +0100

    infra: Start working on auth WIP

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,23 @@
 import { NextResponse } from "next/server";
-
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest): Promise<NextResponse> {
 	if (request.method === "GET") {
-		return NextResponse.next();
+		const response = NextResponse.next();
+		const token = request.cookies.get("session")?.value ?? null;
+		if (token !== null) {
+			response.cookies.set("session", token, {
+				path: "/",
+				maxAge: 60 * 60 * 24 * 30,
+				sameSite: "lax",
+				httpOnly: true,
+				secure: process.env.NODE_ENV === "production"
+			});
+		}
+		return response;
 	}
+
 	const originHeader = request.headers.get("Origin");
-	// NOTE: You may need to use `X-Forwarded-Host` instead
 	const hostHeader = request.headers.get("Host");
 	if (originHeader === null || hostHeader === null) {
 		return new NextResponse(null, {
