34eddc9e25736d33479cb53277734b10187c8505
peterdinis <petter1@pobox.sk>
2025-02-09 11:01:00 +0100

    feat: Remove theme-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navigation from "./_components/shared/Navigation";
 import { Toaster } from "@/components/ui/toaster"
-import { ThemeProvider } from "./_components/shared/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,15 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider 
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange>
           <Navigation />
           {children}
           <Toaster />
-        </ThemeProvider>
       </body>
     </html>
   );
