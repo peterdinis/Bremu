ffe4d43e99ade287a7ded1e70f5a645066c6f01f
peterdinis <petter1@pobox.sk>
2025-02-09 12:26:04 +0100

    infra: Formatting files

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,18 +1,18 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { shippingAddress, orderItem, order } from './order'
-import { product } from './product'
-import { productCategory } from './product-category'
-import { promotionCampaign } from './promotion-campaign'
-import { promotionCode } from './promotion-codes'
+import type { SchemaTypeDefinition } from "sanity";
+import { order, orderItem, shippingAddress } from "./order";
+import { product } from "./product";
+import { productCategory } from "./product-category";
+import { promotionCampaign } from "./promotion-campaign";
+import { promotionCode } from "./promotion-codes";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [
-    promotionCode,
-    promotionCampaign,
-    productCategory,
-    product,
-    shippingAddress,
-    orderItem,
-    order,
-  ],
-}
+	types: [
+		promotionCode,
+		promotionCampaign,
+		productCategory,
+		product,
+		shippingAddress,
+		orderItem,
+		order,
+	],
+};
